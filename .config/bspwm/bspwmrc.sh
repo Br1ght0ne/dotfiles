f6999a0d477d8e043a20daa83b334061503f2ec5
Oleksii Filonenko <brightone@protonmail.com>
2019-06-06 10:02:55 +0300

    Updated bspwm config
    - cursor
    - flashfocus
    - rules

--- .config/bspwm/bspwmrc.sh
+++ .config/bspwm/bspwmrc.sh
@@ -4,31 +4,23 @@ scripts="$HOME/.local/bin"
 
 "$scripts/bspwm-monitor.sh"
 wmname LG3D
+xsetroot -cursor_name left_ptr
 
-bspc config border_width           3
-bspc config window_gap             5
-bspc config split_ratio            0.5
-bspc config borderless_monocle     true
-bspc config gapless_monocle        true
-bspc config single_monocle         true
-bspc config initial_polarity       second_child
-bspc config click_to_focus         any
+# pip3 install --user flashfocus
+# bspc config border_width            0
+bspc config border_width            2
+bspc config window_gap              5
+bspc config split_ratio             0.5
+bspc config borderless_monocle      true
+bspc config gapless_monocle         true
+bspc config single_monocle          true
+bspc config initial_polarity        second_child
+bspc config click_to_focus          any
+bspc config pointer_follows_monitor true
+
+bspc desktop 二 -l monocle
+bspc desktop 五 -l monocle
 
 bspc config external_rules_command "$scripts/bspwm_external_rules"
-# bspc rule -a mpv-cam                            state=floating,sticky
-# bspc rule -a mpv                                state=fullscreen
-# bspc rule -a qutebrowser        desktop='II'    follow=off
-# bspc rule -a Emacs              desktop='I'     state=tiled follow=off
-# bspc rule -a libreoffice                        state=tiled
-# bspc rule -a Wire               desktop='V'     follow=off
-# bspc rule -a Riot               desktop='V'     follow=off
-# bspc rule -a dropdown                           state=sticky
-# bspc rule -a Pavucontrol                        state=floating
-# bspc rule -a st-floating                        state=floating
-# bspc rule -a Pinentry                           state=floating
-# bspc rule -a Transmission-gtk   desktop='X'     follow=off
-# bspc rule -a Screenkey manage=off
-# bspc rule -a Zathura                            state=tiled
-# bspc rule -a libreoffice-writer                 state=tiled
 
 "$scripts/bootstrap"
--- .local/bin/bspwm-monitor.sh
+++ .local/bin/bspwm-monitor.sh
@@ -3,11 +3,11 @@
 ext=$(xrandr | grep 'VGA1 connected')
 if [ -n "$ext" ]; then
 	echo ext
-	bspc monitor LVDS1 -d trm1 webs code
-	bspc monitor VGA1 -d vids stff chat trm2 trm3 news serv
+	bspc monitor LVDS1 -d 一
+	bspc monitor VGA1 -d 二 三 四 五 六 七 八 九 十
 	exit 0
 fi
 
 echo noext
-bspc monitor -d I II III IV V VI VII VIII IX X
-
+# bspc monitor -d I II III IV V VI VII VIII IX X
+bspc monitor -d 一 二 三 四 五 六 七 八 九 十
