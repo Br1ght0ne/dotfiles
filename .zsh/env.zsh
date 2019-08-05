65bc63788a797e11da6a00d6080275b8ca938ee3
Oleksii Filonenko <brightone@protonmail.com>
2019-08-05 23:28:59 +0300

    Add config for {podcast,ripgrep} (Rust)

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
 export BROWSER="firefox-bin"
+
+# Podcast directory for `podcast` - a command-line podcast manager.
+# https://crates.io/crates/podcast
+export PODCAST="$HOME/podcasts"
+
+export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
