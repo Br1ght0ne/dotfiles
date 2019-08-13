c5f21ba9fab8d9f7537d6b24146974657dce13db
Oleksii Filonenko <brightone@protonmail.com>
2019-08-13 14:28:19 +0300

    Add token for brew

--- .zsh/brew.zsh
+++ .zsh/brew.zsh
@@ -1,3 +1,4 @@
 export HOMEBREW_AUTO_UPDATE_SECS=86400 # don't update more often than every day
 export HOMEBREW_EDITOR="vim"
+export HOMEBREW_GITHUB_API_TOKEN=$(pass show tokens/github/homebrew)
 export MANPATH="/home/linuxbrew/.linuxbrew/Homebrew/manpages:$MANPATH"
