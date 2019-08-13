f7b3e93a8165f283a186c9a22cc215dfd8287c64
Oleksii Filonenko <brightone@protonmail.com>
2019-08-13 14:28:05 +0300

    Setup n function for nnn

---
+++ .zsh/nnn.zsh
@@ -0,0 +1,10 @@
+n() {
+    export NNN_TMPFILE=${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd
+
+    nnn "$@"
+
+    if [ -f $NNN_TMPFILE ]; then
+            . $NNN_TMPFILE
+            rm $NNN_TMPFILE
+    fi
+}
