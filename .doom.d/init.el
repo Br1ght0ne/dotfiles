e9d564d1d8bf7c36ba7b0b13625f29bd24367c7e
Oleksii Filonenko <brightone@protonmail.com>
2019-08-30 15:36:28 +0300

    Enable window numbers

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -162,7 +162,8 @@
        unicode            ; extended unicode support for various languages
        vc-gutter          ; vcs diff in the fringe
        ;; vi-tilde-fringe ; fringe tildes to mark beyond EOB
-       window-select      ; visually switch windows
+       (window-select     ; visually switch windows
+        +numbers)         ; jump around like a freak
        workspaces         ; tab emulation, persistence & separate workspaces
 
        :config            ; configs. configs always change
