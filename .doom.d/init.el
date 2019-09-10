4b32e88622f05a34ee88717b5c5711738e1be842
Oleksii Filonenko <brightone@protonmail.com>
2019-09-10 13:03:08 +0300

    Add Mac variables to init.el

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -176,6 +176,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mouse-wheel-progressive-speed nil)
  '(safe-local-variable-values
    '((TeX-engine . xelatex)
      (org-cycle-include-plain-lists . integrate)
